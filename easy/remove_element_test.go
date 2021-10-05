895b52b06fc4ff83c27db3ef9d5bef3821ed7d00
tuannguyen1 <tuannguyen1@beeketing.net>
2021-10-05 15:16:03 +0700

    remove-element

---
+++ easy/remove_element_test.go
@@ -0,0 +1,51 @@
+package easy
+
+import "testing"
+
+/*
+ * https://leetcode.com/problems/remove-element/
+ * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+ * Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+ * Return k after placing the final result in the first k slots of nums.
+ * Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+ */
+
+func TestRemoveElement(t *testing.T) {
+	// Example 1
+	nums := []int{3, 2, 2, 3}
+	value := 3
+	output := 2
+
+	result := removeElement(nums, value)
+
+	if result != output {
+		t.Error("Failed example 1")
+	}
+
+	// Example 2
+	nums = []int{0, 1, 2, 2, 3, 0, 4, 2}
+	value = 2
+	output = 5
+
+	result = removeElement(nums, value)
+
+	if result != output {
+		t.Error("Failed example 2")
+	}
+}
+
+func removeElement(nums []int, val int) int {
+	unique := make(map[int]bool)
+	unique[val] = true
+	arr := make([]int, 0)
+	for _, value := range nums {
+		if _, ok := unique[value]; !ok {
+			arr = append(arr, value)
+		}
+	}
+
+	for k, v := range arr {
+		nums[k] = v
+	}
+	return len(arr)
+}
